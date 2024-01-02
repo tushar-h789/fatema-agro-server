dd7268eface20d0b236ad9f681a2dab5cde5f215
tushar-dev001 <tushar789imran@gmail.com>
2024-01-03 02:08:22 +0600

    cart api done

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-const express = require('express')
-const app = express()
-const cors = require('cors');
-const port = process.env.PORT || 5000
-require('dotenv').config()
-
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port = process.env.PORT || 5000;
+require("dotenv").config();
 
 //middleware
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 //fatema-agro
 // CNmUK07hLG82BkFI
@@ -16,7 +15,7 @@ console.log(process.env.DB_PASS);
 
 //mongoDB setup
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dshv9hb.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -25,16 +24,49 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    //collections
+    const productCollection = client.db("fatema-agro").collection("products");
+    const cartCollection = client.db("fatema-agro").collection("carts");
+
+    app.get("/products", async (req, res) => {
+      const result = await productCollection.find().toArray();
+      res.send(result);
+    });
+
+    //carts collection
+    app.get("/carts", async (req, res) => {
+      const email = req.query.email;
+      const query = {email:email}
+      const result = await cartCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    app.post("/carts", async (req, res) => {
+      const cartItem = req.body;
+      const result = await cartCollection.insertOne(cartItem);
+      res.send(result);
+    });
+
+    app.delete('/carts/:id', async(req, res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await cartCollection.deleteOne(query)
+      res.send(result)
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -42,11 +74,10 @@ async function run() {
 }
 run().catch(console.dir);
 
+app.get("/", (req, res) => {
+  res.send("fatema agro is running");
+});
 
-app.get('/', (req, res)=>{
-    res.send('fatema agro is running')
-})
-
-app.listen(port, ()=>{
-    console.log(`Fatema Agro is running on port ${port}`);
-})
+app.listen(port, () => {
+  console.log(`Fatema Agro is running on port ${port}`);
+});
