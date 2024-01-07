a7cd3ec7bb6b44f99102c1dbe4091900f1c24625
tushar-dev001 <tushar789imran@gmail.com>
2024-01-07 13:27:47 +0600

    question delete button api create

--- index.js
+++ index.js
@@ -261,6 +261,13 @@ async function run() {
       res.send(result);
     });
 
+    app.delete('/usersQuestion/:id', async(req, res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await questionCollection.deleteOne(query)
+      res.send(result)
+    })
+
     app.patch(
       "/usersQuestion/:id",
       verifyToken,
